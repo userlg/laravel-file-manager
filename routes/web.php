f88f72989aa1600b6e0eb30ad9162115e870f0df
userlg <guipeluis90@gmail.com>
2024-01-14 10:37:13 -0400

    New Config file system

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,11 @@
 use App\Http\Controllers\Web\FileManagerController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+
+    return to_route('filemanager.index');
+});
+
 Route::controller(FileManagerController::class)->group(function () {
     Route::get('filemanager', 'index')->name('filemanager.index');
 });
